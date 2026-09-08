c28b8ccc65b1cc31cc9d550b1aa062771bc26872
Morelia-Gonzalez <moreliagonzalezixcot@gmail.com>
2026-09-07 22:32:13 -0600

    .
    .

--- Nueva carpeta/obj/Debug/net8.0-windows/BibliotecaApp.AssemblyInfo.cs
+++ Nueva carpeta/obj/Debug/net8.0-windows/BibliotecaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9202a171743919e7d73b466d50e157626c9458")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+609eef9be3ea94c96d8cbc790145c78ecac7b353")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
